54388152e0684f04fff2ebbe34f1e014f7ef109c
Adam Short <adam010101@icloud.com>
2017-02-06 02:31:50 +0000

    Make API

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => res.send("Hello world!"));
 
-app.listen(8080, () => console.log("Listening on port 8080."));
+app.get('/:date', (req, res) => {
+    var dateParameter = req.params["date"]
+    var response = {dateString: null, timeStamp: null};
+    var myDate;
+    
+    if (!isNaN(dateParameter)) {
+       myDate = new Date(parseInt(dateParameter)*1000);
+    } else if (/\d{1,2}\+\w{3,}\+\d{4}/.test(dateParameter)) {
+        myDate = new Date(Date.parse(dateParameter.replace(/\+/g, " ")));
+    } else {
+        res.send(response); return;
+    }
+    
+    response.dateString = myDate.toDateString();
+    response.timeStamp = myDate.getTime() / 1000;
+    res.send(response);
+});
+
+app.listen('8080', () => console.log("Listening."))
